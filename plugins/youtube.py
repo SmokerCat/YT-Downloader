c62dfbef12014a009003334678b203632e5b2791
CAt <80395140+SmokerCat@users.noreply.github.com>
2021-06-24 20:36:07 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -28,10 +28,10 @@ async def ytdl(_, message):
                                      timedelta(minutes=youtube_next_fetch)
 
     except Exception:
-        await message.reply_text("`A fail tlat mai.. 😔 \nA chhhan chu, he Video Data hi ka hmu thei lo tlat. \n#error`")
+        await message.reply_text("`Something went error... 😔 \nPlease report this on @Cat_Telegram_Project_Club. \n#error`")
         return
     buttons = InlineKeyboardMarkup(list(create_buttons(formats)))
-    sentm = await message.reply_text("YT URL a dik e... © @TGBotSzK")
+    sentm = await message.reply_text("Youtube Link...")
     try:
         # Todo add webp image support in thumbnail by default not supported by pyrogram
         # https://www.youtube.com/watch?v=lTTajzrSkCw
@@ -39,7 +39,7 @@ async def ytdl(_, message):
         await sentm.delete()
     except Exception as e:
         try:
-            thumbnail_url = "https://telegra.ph/file/d75ec77d3d1a4a7f64548.jpg"
+            thumbnail_url = "https://telegra.ph/file/cb9215f6bdc988f2ff7c8.jpg"
             await message.reply_photo(thumbnail_url, caption=title, reply_markup=buttons)
         except Exception as e:
             await sentm.edit(
